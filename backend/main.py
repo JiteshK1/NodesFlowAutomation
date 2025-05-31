7e9a97a65d2491b7b208271d852974978eda5312
jitesh <kanyaljitesh0@gmail.com>
2025-05-31 23:33:38 +0530

    CORS fix

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 
 origins = [
-    "http://localhost:3000", ]
+    "http://localhost:3000",
+    "https://nodesflowautomation.onrender.com" ]
 
 app.add_middleware(
     CORSMiddleware,
